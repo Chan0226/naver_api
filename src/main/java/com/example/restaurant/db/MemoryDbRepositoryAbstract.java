e93a498eeb56d82b4c6a79cb02606772e724e4de
chan <3233978@naver.com>
2022-08-07 15:41:20 +0900

    finish

--- src/main/java/com/example/restaurant/db/MemoryDbRepositoryAbstract.java
+++ src/main/java/com/example/restaurant/db/MemoryDbRepositoryAbstract.java
@@ -4,37 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-abstract public class MemoryDbRepositoryAbstract<T extends MemoryDbEntity> implements MemoryDbRepositoryIfs<T>{
+abstract public class MemoryDbRepositoryAbstract<T extends MemoryDbEntity> implements MemoryDbRepositoryIfs<T> {
 
     private final List<T> db = new ArrayList<>();
     private int index = 0;
 
     @Override
     public Optional<T> findById(int index) {
-        return db.stream().filter(it->it.getIndex()==index).findFirst();
+        return db.stream().filter(it -> it.getIndex() == index).findFirst();
     }
 
     @Override
     public T save(T entity) {
-
         var optionalEntity = db.stream().filter(it -> it.getIndex() == entity.getIndex()).findFirst();
-        if(optionalEntity.isEmpty()) {
-            // db에 이미 데이터가 없는 경우
+
+        if(optionalEntity.isEmpty()){
+            // db 에 데이터가 없는 경우
             index++;
             entity.setIndex(index);
             db.add(entity);
             return entity;
-        }else{
-            // db에 데이터가 있는 경우
-            var preIndex = optionalEntity.get().getIndex();
-            entity.setIndex(preIndex);     // 해당값 세팅
 
-            deleteById(preIndex);           // 기존의 값 제거
-            db.add(entity);                 // 세팅값 삽입
+        }else{
+            // db 에 이미 데이터가 있는 경우
+            var preIndex = optionalEntity.get().getIndex();
+            entity.setIndex(preIndex);
+
+            deleteById(preIndex);
+            db.add(entity);
             return entity;
         }
     }
-    
+
     @Override
     public void deleteById(int index) {
         var optionalEntity = db.stream().filter(it -> it.getIndex() == index).findFirst();
@@ -44,7 +45,7 @@ abstract public class MemoryDbRepositoryAbstract<T extends MemoryDbEntity> imple
     }
 
     @Override
-    public List<T> listAll() {
+    public List<T> findAll() {
         return db;
     }
 }
